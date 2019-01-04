5507e6c6f6757e4734930bb6769c924241902a7a
ilyakolker <ilyakolker@gmail.com>
2019-01-04 13:56:51 +0200

    removed node_modules

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 const request = require('request');
 
-hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs');
+
 
 
 app.use(express.static(__dirname + '/public'));
@@ -15,7 +12,7 @@ app.get("/ip",(req,res)=>{
   
  
    request(`http://proxycheck.io/v2/${req.query.ip}?key=446704-298402-570002-06g06y?vpn=1&asn=1&tag=proxycheck.io`, function(error, result, body) {
-      res.send(JSON.parse(body))
+      res.send(JSON.parse(body));
     })
   });
     
